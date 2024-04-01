983b824456de69eba7618f4181de1e99c028516e
dev <tranducquynh00000@gmail.com>
2024-04-01 15:22:58 +0700

    event and update state

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,11 +4,21 @@ import React from "react";
 
 class MyComponent extends React.Component{
     state={name: 'quynh', address: 'hcm'};
+
+    handleClick(event){
+        //console.log(event.target);
+        console.log("name", this.state.name);
+        //merge State => react class
+        this.setState({
+            name: 'dev'
+        });
+    }
     //jsx
     render(){
         return(
             <div>
                 {this.state.name}
+                <button onClick={(event)=>{this.handleClick(event)}}>Click me</button>
             </div>
         )
     }
