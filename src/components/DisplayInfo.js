9eb55e9faef7a770536a87f984fed4941bd56e4a
dev <tranducquynh00000@gmail.com>
2024-04-01 22:40:15 +0700

    conditional sentences

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,23 +1,40 @@
 import React from "react";
 
-class DisplayInfo extends React.Component{
-    render(){
+class DisplayInfo extends React.Component {
+    state = {
+        isShow: true,
+    };
+
+    handleOnClick = (event) => {
+        this.setState({
+            isShow: !this.state.isShow,
+        });
+    };
+    render() {
         //destructuring array/object
-        const listUsers = this.props.listUsers; 
-        console.log(listUsers)
+        const listUsers = this.props.listUsers;
+        console.log(listUsers);
         //props => properties
-        return(
+        return (
             <div>
-                {listUsers.map((user) =>{
-                    return(
-                        <div key={user.id}>
-                            <div>{user.name}</div>
-                            <div>{user.age}</div>
-                        </div>
-                    )
-                })}
+                <div onClick={(event) => this.handleOnClick(event)}>
+                    {this.state.isShow === true ? "hide list user" : "Show list user"}
+                </div>
+                {this.state.isShow && (
+                    <div>
+                        {listUsers.map((user) => {
+                            return (
+                                <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+                                    <div>{user.name}</div>
+                                    <div>{user.age}</div>
+                                    <hr />
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
             </div>
-        )
+        );
     }
 }
 
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,23 +2,25 @@
 // function component
 import React from "react";
 import UserInfo from "./UserInfo";
-import DisplayInfo from "./DisplayInfo"
+import DisplayInfo from "./DisplayInfo";
 
-class MyComponent extends React.Component{
+class MyComponent extends React.Component {
     state = {
-        listUsers: [{id: 1, name: "a", age: "10"},
-            {id: 2, name: "b", age: "20"},
-            {id: 3, name: "c", age: "30"},
-        ]
-    }
+        listUsers: [
+            { id: 1, name: "a", age: "10" },
+            { id: 2, name: "b", age: "20" },
+            { id: 3, name: "c", age: "30" },
+        ],
+    };
+
     //jsx
-    render(){
-        return(
-            <div>               
+    render() {
+        return (
+            <div>
                 <UserInfo />
-                <DisplayInfo listUsers = {this.state.listUsers}/>
+                <DisplayInfo listUsers={this.state.listUsers} />
             </div>
-        )
+        );
     }
 }
 
